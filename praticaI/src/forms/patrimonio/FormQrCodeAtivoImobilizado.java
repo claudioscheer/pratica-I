01f57c71afc9e53aebca05d6087652e1cd8998b1
marcosdollis <marcosfelipedollis@gmail.com>
2016-10-19 20:34:54 -0200

    Resolvido conflitos, falta commitar .jar do QRCode

--- praticaI/src/forms/patrimonio/FormQrCodeAtivoImobilizado.java
+++ praticaI/src/forms/patrimonio/FormQrCodeAtivoImobilizado.java
@@ -1,11 +1,11 @@
 package forms.patrimonio;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+//import com.google.zxing.BarcodeFormat;
+//import com.google.zxing.EncodeHintType;
+//import com.google.zxing.WriterException;
+//import com.google.zxing.common.BitMatrix;
+//import com.google.zxing.qrcode.QRCodeWriter;
+//import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import components.JFrameBusca;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -178,37 +178,41 @@ public class FormQrCodeAtivoImobilizado extends JFrameBusca {
     }
 
     private void generateQrCode(String messsage) {
-        if (messsage == null || messsage.isEmpty()) {
-            image = null;
-            return;
-        }
-
-        try {
-            Hashtable hintMap = new Hashtable();
-            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-            hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-            QRCodeWriter qrCodeWriter = new QRCodeWriter();
-            BitMatrix byteMatrix = qrCodeWriter.encode(messsage, BarcodeFormat.QR_CODE,
-                    this.panelImage.getWidth(), this.panelImage.getHeight(), hintMap);
-            int CrunchifyWidth = byteMatrix.getWidth();
-            image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
-                    BufferedImage.TYPE_INT_RGB);
-            image.createGraphics();
-            Graphics2D graphics = (Graphics2D) image.getGraphics();
-            graphics.setColor(Color.WHITE);
-            graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
-            graphics.setColor(Color.BLACK);
-            for (int i = 0; i < CrunchifyWidth; i++) {
-                for (int j = 0; j < CrunchifyWidth; j++) {
-                    if (byteMatrix.get(i, j)) {
-                        graphics.fillRect(i, j, 1, 1);
-                    }
-                }
-            }
-            panelImage.repaint();
-        } catch (WriterException ex) {
-            System.out.println("Erro");
-        }
+        
+        /*========================================
+        FALTA COMMITAR O .JAR dessa biblioteca. DIEGO
+        =============================*/
+//        if (messsage == null || messsage.isEmpty()) {
+//            image = null;
+//            return;
+//        }
+//
+//        try {
+//            Hashtable hintMap = new Hashtable();
+//            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+//            hintMap.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+//            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+//            BitMatrix byteMatrix = qrCodeWriter.encode(messsage, BarcodeFormat.QR_CODE,
+//                    this.panelImage.getWidth(), this.panelImage.getHeight(), hintMap);
+//            int CrunchifyWidth = byteMatrix.getWidth();
+//            image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
+//                    BufferedImage.TYPE_INT_RGB);
+//            image.createGraphics();
+//            Graphics2D graphics = (Graphics2D) image.getGraphics();
+//            graphics.setColor(Color.WHITE);
+//            graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+//            graphics.setColor(Color.BLACK);
+//            for (int i = 0; i < CrunchifyWidth; i++) {
+//                for (int j = 0; j < CrunchifyWidth; j++) {
+//                    if (byteMatrix.get(i, j)) {
+//                        graphics.fillRect(i, j, 1, 1);
+//                    }
+//                }
+//            }
+//            panelImage.repaint();
+//        } catch (WriterException ex) {
+//            System.out.println("Erro");
+//        }
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
