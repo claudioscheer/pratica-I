a383d629ede0280636e195e622a6adcb11859784
andersonseibert <anderson_seibert@hotmail.com>
2016-11-19 15:04:43 -0200

    No Commit Message

---
+++ praticaI/src/utils/IniciarDadosEstoque.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import dao.EstTipoOperacaoDAO;
+import model.CarEstTipoOperacao;
+
+/**
+ *
+ * @author Anderson
+ */
+public class IniciarDadosEstoque {
+
+    private final EstTipoOperacaoDAO tpOpDao = new EstTipoOperacaoDAO();
+    private CarEstTipoOperacao operacao;
+    
+    public void execute() {
+        new Thread(dados1).start();        
+    }
+    private Runnable dados1 = new Runnable() {
+
+        public void run() {
+            operacao = InserirEspecificacao(1, "Entrada", "Para Entradas");
+            operacao = InserirEspecificacao(1, "Saída", "Para Saídas");
+        }
+    };
+
+    public CarEstTipoOperacao InserirEspecificacao(int codigo, String nome, String descricao) {
+
+        CarEstTipoOperacao tpop = new CarEstTipoOperacao();
+        tpop.setTpOpId(codigo);
+        tpop.setTpOpNome(nome);
+        tpop.setTpOpDescricao(descricao);
+        this.tpOpDao.Inserir(tpop);
+        return tpop;
+    }
+}
