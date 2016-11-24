cb24f8c7544a9e6224b737b6f7de5b9fe489d19a
joaooliveira121e <juaooliveira.lp@gmail.com>
2016-11-24 20:38:34 -0200

    No Commit Message

--- praticaI/src/forms/FormEstoque.java
+++ praticaI/src/forms/FormEstoque.java
@@ -17,18 +17,21 @@ import utils.IniciarDadosEstoque;
  *
  * @author Anderson
  */
-public class FormEstoque extends javax.swing.JFrame {
-
+public class FormEstoque extends javax.swing.JFrame
+{
+    
     private EstMovimentacaoDAO movDao = new EstMovimentacaoDAO();
     private int idProduto = 0;
     private String prodString = "";
 
     /**
      * Creates new form FormEstoque
+     *
      * @param parent
      * @param modal
      */
-    public FormEstoque(java.awt.Frame parent, boolean modal) {        
+    public FormEstoque(java.awt.Frame parent, boolean modal)
+    {
         initComponents();
         IniciarDadosEstoque iniciar = new IniciarDadosEstoque();
         iniciar.execute();
@@ -41,7 +44,8 @@ public class FormEstoque extends javax.swing.JFrame {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+    private void initComponents()
+    {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tblMov = new javax.swing.JTable();
@@ -51,6 +55,8 @@ public class FormEstoque extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         txtCustoMedio = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
+        txtTotal = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -62,35 +68,44 @@ public class FormEstoque extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Gerenciamento de Estoque");
         setResizable(false);
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowOpened(java.awt.event.WindowEvent evt) {
+        addWindowListener(new java.awt.event.WindowAdapter()
+        {
+            public void windowOpened(java.awt.event.WindowEvent evt)
+            {
                 formWindowOpened(evt);
             }
         });
 
         tblMov.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
+            new Object [][]
+            {
                 {null, null, null, null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null},
                 {null, null, null, null, null}
             },
-            new String [] {
-                "Data", "Operação", "Quantidade", "Unitário", "Total"
+            new String []
+            {
+                "Data", "Operação", "Quantidade", "Unitário", "Saldo"
             }
-        ) {
-            boolean[] canEdit = new boolean [] {
+        )
+        {
+            boolean[] canEdit = new boolean []
+            {
                 false, false, false, false, false
             };
 
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
+            public boolean isCellEditable(int rowIndex, int columnIndex)
+            {
                 return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(tblMov);
 
-        txtBuscaProduto.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        txtBuscaProduto.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 txtBuscaProdutoActionPerformed(evt);
             }
         });
@@ -101,11 +116,15 @@ public class FormEstoque extends javax.swing.JFrame {
 
         jLabel3.setText("Buscar Produto");
 
+        jLabel4.setText("Saldo");
+
         jMenu1.setText("Cadastros");
 
         jMenuItem1.setText("Tipo de Operação");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 jMenuItem1ActionPerformed(evt);
             }
         });
@@ -116,8 +135,10 @@ public class FormEstoque extends javax.swing.JFrame {
         jMenu2.setText("Relatórios");
 
         jMenuItem3.setText("Gerar Relatório");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 jMenuItem3ActionPerformed(evt);
             }
         });
@@ -128,8 +149,10 @@ public class FormEstoque extends javax.swing.JFrame {
         jMenu3.setText("Lançamentos");
 
         jMenuItem2.setText("Movimentação de Estoque");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
                 jMenuItem2ActionPerformed(evt);
             }
         });
@@ -146,23 +169,33 @@ public class FormEstoque extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 750, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 750, Short.MAX_VALUE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(jLabel1)
+                                .addGap(27, 27, 27)
+                                .addComponent(txtDisponivel, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel4)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jLabel2)
+                                        .addGap(51, 51, 51)
+                                        .addComponent(txtCustoMedio, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                        .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jLabel1)
-                        .addGap(27, 27, 27)
-                        .addComponent(txtDisponivel, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel2)
-                        .addGap(51, 51, 51)
-                        .addComponent(txtCustoMedio, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtBuscaProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 555, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(88, 88, 88))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(txtBuscaProduto, javax.swing.GroupLayout.PREFERRED_SIZE, 555, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(88, 88, 88))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(txtTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap())))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -181,7 +214,11 @@ public class FormEstoque extends javax.swing.JFrame {
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(txtDisponivel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jLabel1)))
-                .addContainerGap(78, Short.MAX_VALUE))
+                .addGap(26, 26, 26)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtTotal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addContainerGap(32, Short.MAX_VALUE))
         );
 
         pack();
@@ -197,13 +234,16 @@ public class FormEstoque extends javax.swing.JFrame {
     private void formWindowOpened(java.awt.event.WindowEvent evt)//GEN-FIRST:event_formWindowOpened
     {//GEN-HEADEREND:event_formWindowOpened
         txtDisponivel.setEditable(false);
+        txtCustoMedio.setEditable(false);
+        txtTotal.setEditable(false);
         FormBuscaProduto buscaProd = new FormBuscaProduto();
         buscaProd.setFrameBloquear(FormPrincipal.getInstance());
         this.txtBuscaProduto.setFrame(buscaProd);
         this.txtBuscaProduto.setFuncaoDepoisSelecionar(produto
-                -> {
-            preencheTabela((EstProduto) produto);
-        });
+                ->
+                {
+                    preencheTabela((EstProduto) produto);
+                });
     }//GEN-LAST:event_formWindowOpened
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jMenuItem2ActionPerformed
@@ -219,45 +259,57 @@ public class FormEstoque extends javax.swing.JFrame {
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         // TODO add your handling code here:
-        FormRelatorioEstoque form = new FormRelatorioEstoque(null, rootPaneCheckingEnabled);
-        form.setVisible(true);
+        //  FormRelatorioEstoque form = new FormRelatorioEstoque(null, rootPaneCheckingEnabled);
+        //   form.setVisible(true);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+        try
+        {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
+            {
+                if ("Nimbus".equals(info.getName()))
+                {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex)
+        {
             java.util.logging.Logger.getLogger(FormEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
+        } catch (InstantiationException ex)
+        {
             java.util.logging.Logger.getLogger(FormEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+        } catch (IllegalAccessException ex)
+        {
             java.util.logging.Logger.getLogger(FormEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (javax.swing.UnsupportedLookAndFeelException ex)
+        {
             java.util.logging.Logger.getLogger(FormEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
                 FormEstoque dialog = new FormEstoque(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                dialog.addWindowListener(new java.awt.event.WindowAdapter()
+                {
                     @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
+                    public void windowClosing(java.awt.event.WindowEvent e)
+                    {
                         System.exit(0);
                     }
                 });
@@ -265,58 +317,72 @@ public class FormEstoque extends javax.swing.JFrame {
             }
         });
     }
-
-    private void preencheTabela(EstProduto produto) {
-
+    
+    private void preencheTabela(EstProduto produto)
+    {
+        
         DefaultTableModel modelo = (DefaultTableModel) tblMov.getModel();
         modelo.setNumRows(0);
 
         //semelhante ao foreach
-        for (EstMovimentacao mov : movDao.findByProduto(produto)) {
-            modelo.addRow(new String[]{
+        for (EstMovimentacao mov : movDao.findByProduto(produto))
+        {
+            modelo.addRow(new String[]
+            {
                 "" + mov.getMovData(),
                 mov.getCarEstTipoOperacao().getTpOpNome(),
                 "" + mov.getMovQuantidade(),
                 "" + mov.getMovVlrUnit(),
-                "" + mov.getMovTotal(),});
-
+                "" + mov.getMovTotal(),
+            });
+            
         }
-
-        txtDisponivel.setText("" + calculaCusto()[0]);
-        txtCustoMedio.setText("" + movDao.doubleDecimais(calculaCusto()[1]));
+        
+        txtDisponivel.setText("" + movDao.format(calculaCusto()[0]).replace(",", "."));
+        txtCustoMedio.setText("" + movDao.format(calculaCusto()[1]).replace(",", "."));
+        txtTotal.setText("" + movDao.format(calculaCusto()[2]).replace(",", "."));
         paint(calculaCusto()[0]);
-
+        
     }
-
-    public double[] calculaCusto() {
-        double vet[] = new double[2];
+    
+    public double[] calculaCusto()
+    {
+        double vet[] = new double[3];
         double qtde = 0.0, custoM = 0.0, total = 0.0;
-        for (EstMovimentacao mov : movDao.getAll()) {
-            if (mov.getCarEstTipoOperacao().getTpOpId() == 1) {
+        for (EstMovimentacao mov : movDao.getAll())
+        {
+            if (mov.getCarEstTipoOperacao().getTpOpId() == 1)
+            {
                 qtde += mov.getMovQuantidade();
                 total += mov.getMovTotal();
-            } else {
+            } else
+            {
                 qtde += (-mov.getMovQuantidade());
                 total += (-mov.getMovTotal());
             }
-
+            
         }
-
-        vet[0] = qtde;
-        vet[1] = (qtde != 0 && total != 0 ? total / qtde : 0);
-
+        custoM = (qtde != 0 && total != 0 ? total / qtde : 0);
+        vet[0] = qtde < 0 ? qtde * -1 : qtde;
+        vet[1] = custoM < 0 ? custoM * -1 : custoM;
+        vet[2] = total < 0 ? total * -1 : total;
+        
         return vet;
     }
-
-    public void paint(double value) {
-        if (value < 0) {
+    
+    public void paint(double value)
+    {
+        if (value < 0)
+        {
             txtDisponivel.setBackground(Color.red);
-        } else if (value == 0.0) {
+        } else if (value == 0.0)
+        {
             txtDisponivel.setBackground(Color.yellow);
-        } else {
+        } else
+        {
             txtDisponivel.setBackground(Color.green);
         }
-
+        
     }
 
 
@@ -324,6 +390,7 @@ public class FormEstoque extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
@@ -336,5 +403,6 @@ public class FormEstoque extends javax.swing.JFrame {
     private components.TextFieldFK txtBuscaProduto;
     private javax.swing.JTextField txtCustoMedio;
     private javax.swing.JTextField txtDisponivel;
+    private javax.swing.JTextField txtTotal;
     // End of variables declaration//GEN-END:variables
 }
